522d6757c3440ceee5a7602d132f265fe357e6e8
luyilin <luyilin12@gmail.com>
2018-01-31 11:19:22 +0800

    update demo

--- example/App.vue
+++ example/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <a href="https://github.com/luyilin/vue-cute-timeline" target="_blank" class="github-corner" aria-label="View source on Github"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:lightblue; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>
     <timeline timelineTheme="lightblue">
-      <timeline-title>人生三大难题</timeline-title>
+      <timeline-title>2018</timeline-title>
       <timeline-item color="#9dd8e0">
-        <p>早上吃啥</p>
-        <p class="append">我选水煎包🙋</p>
+        <p>Welcome to the new year!</p>
       </timeline-item>
-      <timeline-item color="#e6b6b0" :hollow="true">中午吃啥</timeline-item>
-      <timeline-item color="#b0e6d1">晚上吃啥</timeline-item>
-      <timeline-title color="#f2d7e1">看到人生三大难题这么简单我就放心了</timeline-title>
+      <timeline-item color="#e6b6b0" :hollow="true">My first 100 stars on Github 🎉</timeline-item>
+      <timeline-item color="#b0e6d1">keep going</timeline-item>
+      <timeline-title color="#f2d7e1">2017</timeline-title>
       <timeline-item lineColor="#a6ade4">
-        <p>别想了，减肥的日子是没有夜宵的</p>
+        <p>I write <a href="https://github.com/luyilin/Aoba" target="_blank">Aoba</a></p>
+        <p class="append">A tool to create a resume easier.</p>
       </timeline-item>
-      <timeline-item :hollow="true">我藏的零食呢？！！</timeline-item>
+      <timeline-item :hollow="true">
+        <p>And <a href="https://github.com/luyilin/Maltose" target="_blank">Maltose</a></p>
+        <p class="append">A cute emoticon keyboard which can generate random emoticon and no xss.</p>
+      </timeline-item>
     </timeline>
   </div>
 </template>
@@ -33,6 +36,11 @@ export default {
 <style>
   .append {
     font-size: .8em
+  }
+  a {
+    color: #bf6dcf;
+    font-weight: bold;
+    text-decoration: none;
   }
   .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}
 </style>
